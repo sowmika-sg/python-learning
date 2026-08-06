7ead509a02ac1a19c89e02db7689fdc1934464c6
Shiny Sowmika Gandham <sowmikasg.2209@gmail.com>
2026-08-06 05:47:20 +0530

    Add initial Python learning files

---
+++ testLibraries.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_iris
+
+print("NumPy Version:", np.__version__)
+print("Pandas Version:", pd.__version__)
+print("Matplotlib Loaded Successfully")
+print("Seaborn Loaded Successfully")
+
+iris = load_iris()
+print("Machine Learning Dataset Loaded Successfully!")
+
+print("\n🎉 Congratulations!")
+print("Your AI & ML Environment is Ready.")
